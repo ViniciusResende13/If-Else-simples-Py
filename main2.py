77a609d45eb019e2743755c100bbf7c1c5b155b0
Vinicius Resende do Carmo <105870043+ViniciusResende13@users.noreply.github.com>
2022-09-23 13:43:16 -0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,8 @@
+idade= input('Qual é a sua idade')
+peso= input('Qual é o seu peso')
+altura= input('Qual é a sua altura')
+
+if idade >= '18' and peso >= '60' and altura >='1.70':
+    print('Está apto para entrar no exército')
+else :
+    print('NÃO está apto para entrar no exército')
